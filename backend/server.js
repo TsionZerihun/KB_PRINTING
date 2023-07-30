2eb9fa1c696f44838821c95fa78f3b151c9839ac
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-07-30 21:03:43 +0300

    fix error

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-import uploadRouter from "./routes/uploadRoutes.js";
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
@@ -32,7 +31,6 @@ app.get("/api/keys/google", (req, res) => {
   res.send({ key: process.env.GOOGLE_API_KEY || "" });
 });
 
-app.use("/api/upload", uploadRouter);
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
