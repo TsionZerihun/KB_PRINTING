9896339608698790dec65125ad1c5ecce0634c4d
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-07-30 22:42:02 +0300

    changing file

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -3,15 +3,13 @@ import Product from '../models/productModel.js';
 import data from '../data.js';
 import User from '../models/userModel.js';
 
-
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany({});
+  await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-  await User.deleteMany({});
+  await User.remove({});
   const createdUsers = await User.insertMany(data.users);
   res.send({ createdProducts, createdUsers });
-
 });
-export default seedRouter; 
+export default seedRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-import express from "express";
-import path from "path";
-import mongoose from "mongoose";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {np
+  .then(() => {
     console.log("connected to db");
   })
   .catch((err) => {
@@ -31,15 +31,16 @@ app.get("/api/keys/google", (req, res) => {
   res.send({ key: process.env.GOOGLE_API_KEY || "" });
 });
 
+app.use("/api/upload", uploadRouter);
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/portfolio_ecom/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/portfolio_ecom/build/index.html"))
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
